4faafa704efb0d3cd97f2bc3387860a0a7eaba0d
Brittani Lovelace <blovelace84@gmail.com>
2025-02-23 20:47:30 -0500

    No updates

--- cosmic-encyclopedia/src/App.jsx
+++ cosmic-encyclopedia/src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import HomePage from "./HomePage";
@@ -7,14 +8,18 @@ import ContentPage from "./ContentPage";
 function App() {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/planets" element={<CategoryPage category="planets" />} />
-        <Route path="/stars" element={<CategoryPage category="stars" />} />
-        <Route path="/galaxies" element={<CategoryPage category="galaxies" />} />
-        <Route path="/content/:id" element={<ContentPage />} />
-      </Routes>
+      <div className="app-container">
+        <NavBar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/planets" element={<CategoryPage category="planets" />} />
+            <Route path="/stars" element={<CategoryPage category="stars" />} />
+            <Route path="/galaxies" element={<CategoryPage category="galaxies" />} />
+            <Route path="/content/:id" element={<ContentPage />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
